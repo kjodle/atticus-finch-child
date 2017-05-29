5cc1ef6701160eb6d81a32aa0d7adbbfe22b34c3
kjodle <kenneth.odle@gmail.com>
2017-05-29 12:26:36 -0400

    Final version of child theme

--- functions.php
+++ functions.php
@@ -2,13 +2,34 @@
 // Exit if accessed directly
 if ( !defined( 'ABSPATH' ) ) exit;
 
-/* Do NOT remove the below function or you child theme will not load properly */
 
-if ( !function_exists( 'af_childtheme_parent_css' ) ):
-    function af_childtheme_parent_css() {
-        wp_enqueue_style( 'af_childtheme_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array(  ) );
-    }
-endif;
-add_action( 'wp_enqueue_scripts', 'af_childtheme_parent_css', 10 );
+/* Add a Google Webfont by uncommenting the following functions and changing the 'family' attribute below. */
+/*
+function af_childtheme_add_google_fonts() {
+	wp_enqueue_style( 'af-google-fonts', 'http://fonts.googleapis.com/css?family=Asul', false ); 
+	}
+add_action( 'wp_enqueue_scripts', 'af_childtheme_add_google_fonts' );
+*/
+
+/* Let's load our child theme. */
+function atticus_finch_enqueue_styles() {
+	
+	/* Enqueue the parent theme script. Do NOT remove the below function or you child theme will not load properly */
+	wp_enqueue_style( 'atticus-finch-style', get_template_directory_uri() . '/style.css' );
+	
+	/* Now we will load the child theme's style sheet: */
+	wp_enqueue_style( 'atticus-finch-child-style', get_stylesheet_directory_uri() . '/style.css', 'atticus-finch-style', wp_get_theme() -> get( 'Version' ) );
+	
+	/* If you need to make changes to the child theme's mobile styles, uncomment the following line and edit the child theme's '/styles/mobile.css' file. */
+//	wp_enqueue_style( 'atticus-finch-child-mobile-style', get_stylesheet_directory_uri() . '/styles/mobile.css', 'atticus-finch-mobile', wp_get_theme() -> get( 'Version' ), 'screen and (max-width: '. get_theme_mod( 'atticus_finch_mobile_breakpoint' ) . 'px)' );
+	
+	/* If you need to make changes to the child theme's menu styles, uncomment the following line and edit the child theme's '/styles/menu.css' file. */
+//	wp_enqueue_style( 'atticus-finch-child-menu-style', get_stylesheet_directory_uri() . '/styles/menus.css', 'atticus-finch-mobile', wp_get_theme() -> get( 'Version' ) );
+	
+	/* If you need to make changes to the child theme's print styles, uncomment the following line and edit the child theme's '/styles/print.css' file. */
+//	wp_enqueue_style( 'atticus-finch-child-print-style', get_stylesheet_directory_uri() . '/styles/print.css', 'atticus-finch-mobile', wp_get_theme() -> get( 'Version' ), 'print' );
+}
+
+add_action( 'wp_enqueue_scripts', 'atticus_finch_enqueue_styles' );
 
 /* Add your child theme's functions below */
